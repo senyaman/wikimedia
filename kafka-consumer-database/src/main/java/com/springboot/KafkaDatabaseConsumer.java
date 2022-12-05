740d064e583063781fc5b28ac8f8d0439d78e9a7
Senya- Man <masenyagift0@gmail.com>
2022-12-05 16:49:14 +0200

    added entity class and repository layer

--- kafka-consumer-database/src/main/java/com/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/springboot/KafkaDatabaseConsumer.java
@@ -1,16 +1,26 @@
 package com.springboot;
 
+import com.springboot.entity.WikimediaData;
+import com.springboot.repository.WikimediaDataRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaDatabaseConsumer {
+
+    private final WikimediaDataRepository dataRepository;
 
     @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
     public void consume(String eventMessage) {
 
         log.info(String.format("Event message received -> %s", eventMessage));
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepository.save(wikimediaData);
     }
 }
---
+++ kafka-consumer-database/src/main/java/com/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.repository;
+
+import com.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
